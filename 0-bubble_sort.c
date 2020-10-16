54b08592865c854fbae084a852cf584f58bc23b8
serosc95 <oscardleon95@gmail.com>
2020-10-16 21:13:28 +0000

    Corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,5 +6,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, sorted = 0;
+	int aux;
 
+	if (size < 2)
+		return;
+	while (sorted < size - 1)
+	{
+		sorted = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				aux = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux;
+			}
+			else
+				sorted++;
+		}
+	}
 }
