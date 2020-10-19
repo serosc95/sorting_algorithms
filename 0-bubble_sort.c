32c16d96738716dac0dc3db53d84d135d78bcd70
serosc95 <oscardleon95@gmail.com>
2020-10-19 20:15:46 +0000

    bubble Efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,13 +7,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	int aux, sorted = 1;
-	size_t new_size = size, i;
+	size_t i;
 
 	if (array == NULL || size < 2)
 		return;
 	while (sorted)
 	{
-		for (sorted = 0, i = 0; i < new_size - 1; i++)
+		for (sorted = 0, i = 0; i < size - 1; i++)
 		{
 			if (array[i + 1] < array[i])
 			{
@@ -24,6 +24,5 @@ void bubble_sort(int *array, size_t size)
 				sorted = 1;
 			}
 		}
-		new_size--;
 	}
 }
