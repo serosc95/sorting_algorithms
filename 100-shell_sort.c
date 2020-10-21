c786d7b4f75df25fbbdfc599c71adcc4137b81a2
serosc95 <oscardleon95@gmail.com>
2020-10-21 19:16:46 +0000

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,31 +7,36 @@
 void shell_sort(int *array, size_t size)
 {
 	int aux, new_size = size;
-	int i, j, range = new_size / 2;
+	int i, j, range = 1;
 
 	if (array == NULL || size < 2)
 		return;
-	while (range > 1)
+	for (i = 0; i < new_size; i = ((i * 3) + 1))
 	{
-		for (i = 0; i + range - 1 < new_size; i++)
+		if (i < new_size)
+			range = i;
+	}
+	while (range >= 1)
+	{
+		for (i = 0; i + range < new_size; i++)
 		{
-			if (array[i] > array[i + range - 1])
+			if (array[i] > array[i + range])
 			{
-				aux = array[i + range - 1];
-				array[i + range - 1] = array[i];
+				aux = array[i + range];
+				array[i + range] = array[i];
 				array[i] = aux;
-				for (j = i; j - range + 1 >= 0; j--)
+				for (j = i; j - range >= 0; j--)
 				{
-					if (array[j - range + 1] > array[j])
+					if (array[j - range] > array[j])
 					{
-						aux = array[j - range + 1];
-						array[j - range + 1] = array[j];
+						aux = array[j - range];
+						array[j - range] = array[j];
 						array[j] = aux;
 					}
 				}
 			}
 		}
 		print_array(array, size);
-		range = range / 2;
+		range = (range - 1) / 3;
 	}
 }
